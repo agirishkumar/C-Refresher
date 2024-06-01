aa2b5522546651301e1627600310b06f8b7bb65d
agirishkumar <adari.girishkumar@gmail.com>
2024-06-01 17:02:48 -0400

    binary search

--- binarysearch.c
+++ binarysearch.c
@@ -1,28 +1,33 @@
 #include<stdio.h>
 
-int binarySearch(int arr[], int l, int r, int target){
-
-    while (l <= r){
+int binarySearch(int arr[], int l, int r, int target) {
+    while (l <= r) {
         int mid = l + (r - l) / 2;
-        if (arr[mid] == target){
+
+        if (arr[mid] == target) {
             return mid;
-        }
-        else if (arr[mid] < target){
+        } else if (arr[mid] < target) {
             l = mid + 1;
-        }
-        else{
+        } else {
             r = mid - 1;
         }
     }
+
+    return -1; // Target not found
 }
 
-int main(){
-
-    int arr[] = {3,6,9,15,28,36,49,56,63};
-
+int main() {
+    int arr[] = {3, 6, 9, 15, 28, 36, 49, 56, 63};
     int n = sizeof(arr) / sizeof(arr[0]);
     int target = 56;
-    int idx = binarySearch(arr,0, n-1, target);
+
+    int idx = binarySearch(arr, 0, n - 1, target);
+
+    if (idx != -1) {
+        printf("Target found at index %d\n", idx);
+    } else {
+        printf("Target not found in the array.\n");
+    }
 
     return 0;
 }
